3f2db5d431c2932c79f6d3e8d1902bed1f119802
JRK <jessym.reziga@gmail.com>
2024-02-13 11:38:02 +0100

    Update pigpio-irtx-irrx-install.sh

--- pigpio-irtx-irrx-install.sh
+++ pigpio-irtx-irrx-install.sh
@@ -3,57 +3,47 @@
 STEPS=7
 CURSTEP=1
 SLEEPTIME=2
-REMOTES_DIR="~/Remotes"
+REMOTES_DIR="/home/pi/Remotes"
 
 # Download pigpio
 echo "[$CURSTEP/$STEPS] Pigpio downloading..."
 sleep $SLEEPTIME && ((CURSTEP++))
-cd /tmp
-wget https://github.com/joan2937/pigpio/archive/master.zip
-unzip master.zip
-cd pigpio-master
+#cd /tmp && rm -rf master.zip 2>/dev/null && wget https://github.com/joan2937/pigpio/archive/master.zip && unzip master.zip && rm master.zip && cd pigpio-master
 
 # Install pigpio
 echo "[$CURSTEP/$STEPS] Pigpio installation..."
 sleep $SLEEPTIME && ((CURSTEP++))
-make
-make install
+#make && make install
 
 # Configure pigpio daemon
 echo "[$CURSTEP/$STEPS] Pigpio configuration..."
 sleep $SLEEPTIME && ((CURSTEP++))
-systemctl daemon-reload
-systemctl enable pigpiod
-systemctl start pigpiod
+# systemctl daemon-reload && systemctl enable pigpiod && systemctl start pigpiod
 
 # Install irrp.py
 echo "[$CURSTEP/$STEPS] Irpp.py installation..."
 sleep $SLEEPTIME && ((CURSTEP++))
-cd /tmp
-wget https://abyz.me.uk/rpi/pigpio/code/irrp_py.zip
-unzip irrp_py.zip
-chmod +x irpp.py
-mv irpp.py /usr/local/bin/
+#cd /tmp && rm -rf irrp_py.zip irrp.py 2>/dev/null && wget https://abyz.me.uk/rpi/pigpio/code/irrp_py.zip && unzip irrp_py.zip && chmod +x irrp.py && mv irrp.py /usr/local/bin/
 
 # Create /usr/local/bin/irtx script and make it executable
 echo "[$CURSTEP/$STEPS] IRTX script installation"
 sleep $SLEEPTIME && ((CURSTEP++))
 cat > /usr/local/bin/irtx <<EOF
-#!/bin/sh
-REMOTE=$1
-BUTTONS=${@:2}
+#!/bin/bash
+REMOTE=\$1
+BUTTONS=\${@:2}
 
-if [ -z "$REMOTE" ]; then
+if [ -z "\$REMOTE" ]; then
   echo "Error : first argument must be a remote in $REMOTES_DIR directory"
   exit 1
 fi
 
-if [ -z "$BUTTONS" ]; then
+if [ -z "\$BUTTONS" ]; then
   echo "Error : at least one button name is needed"
   exit 1
 fi
 
-/usr/local/bin/irrp.py -p -g 18 -f "$REMOTES_DIR/$REMOTE" $BUTTONS
+/usr/local/bin/irrp.py -p -g 18 -f "$REMOTES_DIR/\$REMOTE" \$BUTTONS
 EOF
 chmod +x /usr/local/bin/irtx
 
@@ -61,27 +51,25 @@ chmod +x /usr/local/bin/irtx
 echo "[$CURSTEP/$STEPS] IRRX script installation"
 sleep $SLEEPTIME && ((CURSTEP++))
 cat > /usr/local/bin/irrx <<EOF
-#!/bin/sh
-REMOTE=$1
-BUTTON=${@:2}
+#!/bin/bash
+REMOTE=\$1
+BUTTONS=\${@:2}
 
-if [ -z "$REMOTE" ]; then
+if [ -z "\$REMOTE" ]; then
   echo "Error : first argument must be a remote in $REMOTES_DIR directory"
   exit 1
 fi
 
-if [ -z "$BUTTONS" ]; then
+if [ -z "\$BUTTONS" ]; then
   echo "Error : at least one button name is needed"
   exit 1
 fi
 
-/usr/local/bin/irrp.py -r -g 17 -f "$REMOTES_DIR/$REMOTE" $BUTTONS
+/usr/local/bin/irrp.py -r -g 17 -f "$REMOTES_DIR/\$REMOTE" \$BUTTONS
 EOF
 chmod +x /usr/local/bin/irrx
 
 # Create Remotes directory
 echo "[$CURSTEP/$STEPS] Remote directory"
 sleep $SLEEPTIME && ((CURSTEP++))
-mkdir $REMOTES_DIR 2>/dev/null
-echo "# Remotes
-cd $REMOTES_DIR && ls -1
+mkdir $REMOTES_DIR 2>/dev/null && echo "# Remotes" && cd $REMOTES_DIR && ls -1

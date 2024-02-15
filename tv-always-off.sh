ea01abb5a691791f9e865dfe6d18d8341bf25b5b
JRK <jessym.reziga@gmail.com>
2024-02-15 11:40:05 +0100

    Create tv-always-off.sh

---
+++ tv-always-off.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SLEEPTIME=5
+
+echo "==================="
+echo "TV Always Off Daemon"
+echo "==================="
+
+echo "Wait..."
+sleep 3
+echo "Forever loop..."
+
+while true; do
+  tv_state=$(echo 'pow 0' | cec-client -s -d 1)
+
+  if [[ $tv_state == *'status: on'* ]]; then
+    echo "TV is ON, trying to power off..."
+    ./tv-power-on.sh
+  else
+    echo "TV is OFF."
+  fi
+
+  sleep $SLEEPTIME
+done
